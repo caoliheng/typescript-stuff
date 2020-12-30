128e19cbd0715fa3242b1275fd4a0823316645c0
Liheng <lihengcao@nyu.edu>
2020-12-30 16:09:06 -0600

    ~

--- main.js
+++ main.js
@@ -2,3 +2,17 @@
 exports.__esModule = true;
 var str = 'asdf';
 console.log(str);
+var b = true;
+var n = 0;
+var name = '';
+function add(n1, n2) {
+    return n1 + n2;
+}
+var C = /** @class */ (function () {
+    function C() {
+    }
+    return C;
+}());
+function f(c) {
+    return;
+}
--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
-export {}
+export {} // make this a module
 
 let str = 'asdf';
 console.log(str);
+
+let b :boolean = true;
+let n :number = 0;
+let name :string= ''
+
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+class C{
+    n: number;
+}
+
+function f(c: C){
+    return
+}
